ed4e5b5eff213d37b082677c82d44143de4f95e4
Feniks33 <lucas.chevalier@mail-esd.com>
2020-04-17 04:15:21 +0200

    connexion database

--- functions/createUser.php
+++ functions/createUser.php
@@ -3,25 +3,11 @@
 require("../configs/config.php");
 require("../configs/database.php");
 
-$errorMessage = "";
-
-if($_POST["password"] !== $_POST["confirmPassword"]){
-    $errorMessage = "le mot de passe n'est pas confirmé";
-    header("Location: ../register.php?errorMessage=" . $errorMessage);
-}
-
-if(isset($_POST["role"])){
-    $role_id = 1;
-}else{
-    $role_id = 0;
-}
-
-$req = $dbh->prepare('INSERT INTO user(username, password, role) VALUES(:pseudo, :password, :role)');
+$req = $dbh->prepare('INSERT INTO user(username, password) VALUES(:pseudo, :password)');
 $req->execute(array(
-    'pseudo' => $_POST["pseudo"],
-    'password' => $_POST["password"],
-    'role' => $_POST["role"],
+     'pseudo' => $_POST["pseudo"],
+     'password' => $_POST["password"],
 ));
 
-header("Location: ../index.php");
+// header("Location: ../index.php");
 ?>
--- register.php
+++ register.php
@@ -10,16 +10,6 @@
     <div class="container-fluid" id="form">
         <form action="functions/createUser.php" class="col-md-6" method="post">
             <h1>Créer mon compte</h1>
-
-                <?php if(isset($_GET["errorMessage"])){ ?>
-                    <div class="alert alert-danger alert-dismissible fade show">
-                        <?= $_GET["errorMessage"] ?>
-                        <button class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                <?php } ?>
-
                 <div class="form-group">
                     <input type="text" name="pseudo" class="form-control" placeholder="Pseudo">
                 </div>
@@ -30,7 +20,7 @@
                     <input type="password" name="confirmPassword" class="form-control" placeholder="Password">
                 </div>
                 <div class="form-group form-check">
-                    <input type="checkbox" name="role" class="form-check-input">
+                    <input type="checkbox" name="role" class="form-check-input" id="">
                     <label for="role">Je souhaite devenir un membre du logobi</label>
                 </div>
                 <div class="form-group">
